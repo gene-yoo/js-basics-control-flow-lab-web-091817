c125557c0201d4027ffeb5f57191cc4d98a53d91
gene-yoo <gene.y.yoo@gmail.com>
2017-10-29 22:44:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your code in this file!
+function scuberGreetingForFeet (distance) {
+  let message;
+
+  if (distance <= 400) {
+    message = 'This one is on me!';
+  } else if (distance > 2000 && distance <= 2500) {
+    message = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    message = 'No can do.';
+  } else {
+    message = 'Thanks for riding.';
+  }
+  return message;
+}
+
+function ternaryCheckCity (city) {
+  let message = (city === 'NYC' ? 'Ok, sounds good.' : 'No go.' )
+  return message;
+}
+
+function switchOnCharmFromTip (tip) {
+  let message;
+
+  switch (tip) {
+    case 'generous':
+      message = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      message = 'Thank you.';
+      break;
+    default:
+      message = 'Bye.';
+      break;
+  }
+
+  return message;
+}
